82aed31bf0b5c67b2f1e07e5273842fe28e9e37b
mdobrinic <mdobrinic@2aca1b3b-1dd8-4678-8513-65c153b8ef79>
2012-12-14 17:59:27 +0000

    git-svn-id: svn://svn.code.sf.net/p/asimba/code/trunk@27 2aca1b3b-1dd8-4678-8513-65c153b8ef79

---
+++ asimba-engine-attribute-release-jdbc/src/main/java/com/alfaariss/oa/engine/attribute/release/jdbc/JDBCFactory.java
@@ -0,0 +1,404 @@
+/*
+ * * Asimba - Serious Open Source SSO
+
+ * 
+ * Copyright (C) 2012 Asimba
+ * Copyright (C) 2007-2008 Alfa & Ariss B.V.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see www.gnu.org/licenses
+ * 
+ * * Asimba - Serious Open Source SSO - More information on www.asimba.org
+
+ * 
+ */
+package com.alfaariss.oa.engine.attribute.release.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Element;
+
+import com.alfaariss.oa.OAException;
+import com.alfaariss.oa.SystemErrors;
+import com.alfaariss.oa.api.IComponent;
+import com.alfaariss.oa.api.configuration.ConfigurationException;
+import com.alfaariss.oa.api.configuration.IConfigurationManager;
+import com.alfaariss.oa.api.datastorage.IDataStorageFactory;
+import com.alfaariss.oa.engine.core.Engine;
+import com.alfaariss.oa.engine.core.attribute.AttributeException;
+import com.alfaariss.oa.engine.core.attribute.release.IAttributeReleasePolicy;
+import com.alfaariss.oa.engine.core.attribute.release.factory.IAttributeReleasePolicyFactory;
+import com.alfaariss.oa.util.database.DatabaseException;
+import com.alfaariss.oa.util.database.jdbc.DataSourceFactory;
+
+/**
+ * Release policy factory.
+ *
+ * Reads the policy information from jdbc.
+ * @author MHO
+ * @author Alfa & Ariss
+ */
+public class JDBCFactory implements IAttributeReleasePolicyFactory, IComponent 
+{
+    private final static String TABLE_NAME_POLICY = "attributerelease_policy";
+    private final static String TABLE_NAME_ATTRIBUTE = "attributerelease_expression";
+    
+    private static Log _logger;
+    private IConfigurationManager _configurationManager;
+    private DataSource _oDataSource;
+    private String _sPolicyTable;
+    private String _sAttributeTable;
+    private boolean _bEnabled;
+    
+    /**
+     * Creates the object. 
+     */
+    public JDBCFactory()
+    {
+        _logger = LogFactory.getLog(JDBCFactory.class);
+        _sPolicyTable = null;
+        _sAttributeTable = null;
+        _bEnabled = false;
+    }
+    
+    /**
+     * Returns the policy with the supplied name or <code>null</code> if 
+     * it does not exist.
+     * @see IAttributeReleasePolicyFactory#getPolicy(java.lang.String)
+     */
+    public IAttributeReleasePolicy getPolicy(String policy) 
+        throws AttributeException
+    {
+        IAttributeReleasePolicy oPolicy = null;
+        Connection oConnection = null;
+        PreparedStatement oPreparedStatement = null;
+        ResultSet oResultSet = null;
+        try
+        {
+            oConnection = _oDataSource.getConnection();
+            StringBuffer sbSelect = new StringBuffer("SELECT * FROM ");
+            sbSelect.append(_sPolicyTable);
+            sbSelect.append(" WHERE ");
+            sbSelect.append(JDBCPolicy.COLUMN_POLICY_ID);
+            sbSelect.append("=?");
+            
+            oPreparedStatement = oConnection.prepareStatement(sbSelect.toString());
+            oPreparedStatement.setString(1, policy);
+            oResultSet = oPreparedStatement.executeQuery();
+            if (oResultSet.next())
+                oPolicy = new JDBCPolicy(_oDataSource, oResultSet, _sAttributeTable);
+            
+            _logger.debug("Retrieved profile: " + oPolicy);
+        }
+        catch(Exception e)
+        {
+            _logger.fatal("Internal error during retrieval of policy: " 
+                + policy, e);
+            throw new AttributeException(SystemErrors.ERROR_INTERNAL, e);
+        }
+        finally
+        {
+            try
+            {
+                if (oResultSet != null)
+                    oResultSet.close();
+            }
+            catch (Exception e)
+            {
+                _logger.error("Could not close resultset", e);
+            }
+            
+            try
+            {
+                if (oPreparedStatement != null)
+                    oPreparedStatement.close();
+            }
+            catch (Exception e)
+            {
+                _logger.error("Could not close statement", e);
+            }
+            
+            try
+            {
+                if (oConnection != null)
+                    oConnection.close();
+            }
+            catch (Exception e)
+            {
+                _logger.error("Could not close connection", e);
+            }
+        }
+        return oPolicy;
+    }
+
+    /**
+     * Returns TRUE if this release policy factory is enabled.
+     * @see com.alfaariss.oa.api.IOptional#isEnabled()
+     */
+    public boolean isEnabled()
+    {
+        return _bEnabled;
+    }
+
+    /**
+     * Restarts the release policy factory.
+     * @see com.alfaariss.oa.api.IComponent#restart(org.w3c.dom.Element)
+     */
+    public void restart(Element config) throws OAException
+    {
+        synchronized (this)
+        {
+            stop();
+            start(_configurationManager, config);
+        }
+    }
+
+    /**
+     * Initializes the release policy factory.
+     * @see IComponent#start(IConfigurationManager, org.w3c.dom.Element)
+     */
+    public void start(IConfigurationManager configurationManager, Element config)
+        throws OAException
+    {
+        Connection oConnection = null;
+        try
+        {
+            _bEnabled = false;
+            _configurationManager = configurationManager;
+            
+            Element eResource = _configurationManager.getSection(config, "resource");
+            if (eResource == null)
+            {
+                _logger.warn("No 'resource' section found in configuration, using default table names");
+                _sPolicyTable = TABLE_NAME_POLICY;
+                _sAttributeTable = TABLE_NAME_ATTRIBUTE;
+                
+                IDataStorageFactory databaseFactory = Engine.getInstance().getStorageFactory();
+                if (databaseFactory != null && databaseFactory.isEnabled())
+                {
+                    _oDataSource = databaseFactory.createModelDatasource();
+                    if (_oDataSource == null)
+                    {
+                        _logger.error("Could not create a valid datasource");
+                        throw new DatabaseException(SystemErrors.ERROR_INIT);
+                    }
+                    _logger.info("Using datasource specified in engine");
+                }
+                else
+                {
+                    _logger.error("Could not create a valid datasource");
+                    throw new DatabaseException(SystemErrors.ERROR_INIT);
+                }
+            }
+            else
+            {
+                try
+                {
+                    _oDataSource = DataSourceFactory.createDataSource(_configurationManager, eResource);
+                    _logger.info("Using datasource specified in 'resource' section in configuration");
+                }
+                catch (DatabaseException e)
+                {
+                    IDataStorageFactory databaseFactory = Engine.getInstance().getStorageFactory();
+                    if (databaseFactory != null && databaseFactory.isEnabled())
+                    {
+                        _oDataSource = databaseFactory.createModelDatasource();
+                        if (_oDataSource == null)
+                        {
+                            _logger.error("Could not create a valid datasource");
+                            throw new DatabaseException(SystemErrors.ERROR_INIT);
+                        }
+                        _logger.info("Using datasource specified in engine");
+                    }
+                    else
+                    {
+                        _logger.error("Could not create a valid datasource", e);
+                        throw new DatabaseException(SystemErrors.ERROR_INIT);
+                    }
+                }
+            }
+            
+            try
+            {
+                oConnection = _oDataSource.getConnection();
+            }
+            catch (SQLException e)
+            {
+                _logger.error("Could not connect to resource", e);
+                throw new DatabaseException(SystemErrors.ERROR_INIT, e);
+            }
+            
+            if (eResource != null)
+            {
+                Element ePolicy = _configurationManager.getSection(eResource, "policy");
+                if (ePolicy == null)
+                {
+                    _sPolicyTable = TABLE_NAME_POLICY;
+                    _logger.warn("No 'policy' section found in configuration, using default table: " + _sPolicyTable);
+                }
+                else
+                {
+                    _sPolicyTable = _configurationManager.getParam(ePolicy, "table");
+                    if (_sPolicyTable == null)
+                    {
+                        _sPolicyTable = TABLE_NAME_POLICY;
+                        _logger.warn("No 'table' item in 'policy' section found in configuration, using default: " + _sPolicyTable);
+                    }
+                }
+            }
+            
+            
+            Element eValidation = _configurationManager.getSection(config, "validation");
+            StringBuffer sbPolicyVerify = new StringBuffer("SELECT ");
+            sbPolicyVerify.append(JDBCPolicy.COLUMN_POLICY_ID).append(",");
+            sbPolicyVerify.append(JDBCPolicy.COLUMN_POLICY_FRIENDLYNAME).append(",");
+            sbPolicyVerify.append(JDBCPolicy.COLUMN_POLICY_ENABLED);
+            sbPolicyVerify.append(" FROM ");
+            sbPolicyVerify.append(_sPolicyTable);
+            sbPolicyVerify.append(" LIMIT 1"); 
+            validateTable(oConnection, eValidation, "policy", sbPolicyVerify.toString());
+                       
+            if (eResource != null)
+            {
+                Element eAttribute = _configurationManager.getSection(eResource, "attribute");
+                if (eAttribute == null)
+                {
+                    _sAttributeTable = TABLE_NAME_ATTRIBUTE;
+                    _logger.warn("No 'attribute' section found in configuration, using default table: " + _sAttributeTable);
+                }
+                else
+                {
+                    _sAttributeTable = _configurationManager.getParam(eAttribute, "table");
+                    if (_sAttributeTable == null)
+                    {
+                        _sAttributeTable = TABLE_NAME_ATTRIBUTE;
+                        _logger.warn("No 'table' item in 'attribute' section found in configuration, using default: " + _sAttributeTable);
+                    }
+                }
+            }
+            
+            StringBuffer sbAttributeVerify = new StringBuffer("SELECT ");
+            sbAttributeVerify.append(JDBCPolicy.COLUMN_ATTRIBUTE_POLICY_ID).append(",");
+            sbAttributeVerify.append(JDBCPolicy.COLUMN_ATTRIBUTE_ATTRIBUTE);
+            sbAttributeVerify.append(" FROM ");
+            sbAttributeVerify.append(_sAttributeTable);
+            sbAttributeVerify.append(" LIMIT 1"); 
+            validateTable(oConnection, eValidation, "attribute", sbAttributeVerify.toString());
+            _bEnabled = true;
+        }
+        catch (OAException e)
+        {
+            throw e;
+        }
+        catch(Exception e)
+        {
+            _logger.fatal("Internal error during initialization", e);
+            throw new OAException(SystemErrors.ERROR_INTERNAL, e);
+        }
+        finally
+        {
+            try
+            {
+                if (oConnection != null)
+                    oConnection.close();
+            }
+            catch (Exception e)
+            {
+                _logger.error("Could not close connection", e);
+            }
+        }
+    }
+
+    /**
+     * Stops the release policy factory.
+     * @see com.alfaariss.oa.api.IComponent#stop()
+     */
+    public void stop()
+    {
+        //nothing to do
+    }
+
+    //TODO move to utility class
+    private void validateTable(Connection oConnection, Element eValidation, String table, 
+        String sDefault) throws DatabaseException, SQLException
+    {
+        String sVerificationQuery = null;
+        PreparedStatement pVerification = null;
+        try
+        {
+            if(eValidation != null)
+            {
+                Element e = _configurationManager.getSection(eValidation, table);
+                if(e != null)
+                {
+                    sVerificationQuery = _configurationManager.getParam(e, "query");
+                    if(sVerificationQuery == null || sVerificationQuery.length() == 0)
+                    {
+                        //DD Do not verify the table if empty query configured
+                        _logger.warn("Empty validation query found, table structure is not validated for table:  " + table);
+                        //finally is executed before return
+                        return;
+                    }
+                    _logger.info("Validation query found: " + sVerificationQuery);
+                }
+            }
+            
+            if(sVerificationQuery == null)
+            {
+                //DD Use default query if no query parameter configured                               
+                sVerificationQuery = sDefault;
+                _logger.info("No validation query found, using default: " + sDefault);
+            }
+            
+            pVerification = oConnection.prepareStatement(sVerificationQuery);
+            try
+            {
+                pVerification.executeQuery();
+            }
+            catch(Exception e)
+            {
+                StringBuffer sbError = new StringBuffer("Invalid table configured '");
+                sbError.append(table);
+                sbError.append("' verified with query: ");
+                sbError.append(sVerificationQuery);
+                _logger.error(sbError.toString(), e);
+                throw new DatabaseException(SystemErrors.ERROR_INIT);
+            }        
+            _logger.info("Table structure validated for table: " + table);
+        }
+        catch(ConfigurationException e)
+        {            
+            _logger.error("Invalid validation query found for table: " + table, e);
+            throw new DatabaseException(SystemErrors.ERROR_CONFIG_READ);
+        }
+        finally
+        {
+            try
+            {
+                if (pVerification != null)
+                    pVerification.close();
+            }
+            catch (Exception e)
+            {
+                _logger.error("Could not close verification statement", e);
+            }
+        }
+    } 
+}
---
+++ asimba-engine-attribute-release-jdbc/src/main/java/com/alfaariss/oa/engine/attribute/release/jdbc/JDBCPolicy.java
@@ -0,0 +1,275 @@
+/*
+ * * Asimba - Serious Open Source SSO
+
+ * 
+ * Copyright (C) 2012 Asimba
+ * Copyright (C) 2007-2008 Alfa & Ariss B.V.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see www.gnu.org/licenses
+ * 
+ * * Asimba - Serious Open Source SSO - More information on www.asimba.org
+
+ * 
+ */
+package com.alfaariss.oa.engine.attribute.release.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.alfaariss.oa.SystemErrors;
+import com.alfaariss.oa.api.attribute.IAttributes;
+import com.alfaariss.oa.engine.core.attribute.AttributeException;
+import com.alfaariss.oa.engine.core.attribute.UserAttributes;
+import com.alfaariss.oa.engine.core.attribute.release.IAttributeReleasePolicy;
+
+/**
+ * Release policy class.
+ *
+ * Reads configuration from the configuration document and matches the 
+ * attribute names with or without wildcard (*).
+ * @author MHO
+ * @author Alfa & Ariss
+ */
+public class JDBCPolicy implements IAttributeReleasePolicy
+{
+    /** id */
+    public static final String COLUMN_POLICY_ID = "id";
+    /** friendlyname */
+    public static final String COLUMN_POLICY_FRIENDLYNAME = "friendlyname";
+    /** enabled */
+    public static final String COLUMN_POLICY_ENABLED = "enabled";
+    /** attribute */
+    public static final String COLUMN_ATTRIBUTE_POLICY_ID = "policy_id";
+    /** attribute */
+    public static final String COLUMN_ATTRIBUTE_ATTRIBUTE = "expression";
+
+    private static Log _logger;
+    private String _sID;
+    private String _sFriendlyName;
+    private boolean _bEnabled;
+    private Vector<String> _vAttributeNames;
+    
+    /**
+     * Initializes the policy.
+     * 
+     * @param dataSource the JDBC datasource
+     * @param resultSet A resultset containing a row with policy information
+     * @param attributeTable attribute table name
+     * @throws AttributeException if initialization fails
+     */
+    public JDBCPolicy (DataSource dataSource, ResultSet resultSet,
+        String attributeTable) throws AttributeException
+    {
+        try
+        {
+            _logger = LogFactory.getLog(JDBCPolicy.class);
+            
+            _sID = resultSet.getString(COLUMN_POLICY_ID);
+            _sFriendlyName = resultSet.getString(COLUMN_POLICY_FRIENDLYNAME);
+            _bEnabled = resultSet.getBoolean(COLUMN_POLICY_ENABLED);
+            
+            _vAttributeNames = readAttributes(dataSource, attributeTable);
+        }
+        catch(Exception e)
+        {
+            _logger.fatal("Internal error during initialization", e);
+            throw new AttributeException(SystemErrors.ERROR_INTERNAL);
+        }
+    }
+
+    /**
+     * Applies the policy to the given attributes.
+     * @see com.alfaariss.oa.engine.core.attribute.release.IAttributeReleasePolicy#apply(com.alfaariss.oa.api.attribute.IAttributes)
+     */
+    public IAttributes apply(IAttributes attributes) throws AttributeException
+    {
+        IAttributes oReturnAttributes = new UserAttributes();
+        try
+        {
+            if (_bEnabled)
+            {
+                Enumeration enumNames = attributes.getNames();
+                while (enumNames.hasMoreElements())
+                {
+                    String sName = (String)enumNames.nextElement();
+                    if (matches(sName))
+                        oReturnAttributes.put(sName, 
+                            attributes.getFormat(sName), attributes.get(sName));
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            _logger.fatal("Internal error during apply of release policy: " + _sID, e);
+            throw new AttributeException(SystemErrors.ERROR_INTERNAL);
+        }
+        return oReturnAttributes;
+    }
+
+    /**
+     * This policy its friendly name.
+     * @see com.alfaariss.oa.api.IManagebleItem#getFriendlyName()
+     */
+    public String getFriendlyName()
+    {
+        return _sFriendlyName;
+    }
+
+    /**
+     * This policy its unique ID.
+     * @see com.alfaariss.oa.api.IManagebleItem#getID()
+     */
+    public String getID()
+    {
+        return _sID;
+    }
+
+    /**
+     * Return TRUE if this policy is enabled.
+     * @see com.alfaariss.oa.api.IManagebleItem#isEnabled()
+     */
+    public boolean isEnabled()
+    {
+        return _bEnabled;
+    }
+    
+    /**
+     * @see java.lang.Object#toString()
+     */
+    public String toString()
+    {
+        return _sID;
+    }
+
+    private Vector<String> readAttributes(DataSource dataSource,
+        String attributeTable) throws AttributeException
+    {
+        Vector<String> vAttributeNames = new Vector<String>();
+        Connection oConnection = null;
+        PreparedStatement oPreparedStatement = null;
+        ResultSet oResultSet = null;
+        try
+        {
+            oConnection = dataSource.getConnection();
+            
+            StringBuffer sbSelect = new StringBuffer("SELECT ");
+            sbSelect.append(COLUMN_ATTRIBUTE_ATTRIBUTE);
+            sbSelect.append(" FROM ");
+            sbSelect.append(attributeTable);
+            sbSelect.append(" WHERE ");
+            sbSelect.append(COLUMN_ATTRIBUTE_POLICY_ID);
+            sbSelect.append("=?");
+            
+            oPreparedStatement = oConnection.prepareStatement(sbSelect.toString());
+            oPreparedStatement.setString(1, _sID);
+            oResultSet = oPreparedStatement.executeQuery();
+            while (oResultSet.next())
+            {
+                vAttributeNames.add(oResultSet.getString(COLUMN_ATTRIBUTE_ATTRIBUTE));
+            }
+        }
+        catch(Exception e)
+        {
+            _logger.fatal("Internal error during initialization", e);
+            throw new AttributeException(SystemErrors.ERROR_INTERNAL);
+        }
+        finally
+        {
+            try
+            {
+                if (oResultSet != null)
+                    oResultSet.close();
+            }
+            catch (Exception e)
+            {
+                _logger.error("Could not close resultset", e);
+            }
+            
+            try
+            {
+                if (oPreparedStatement != null)
+                    oPreparedStatement.close();
+            }
+            catch (Exception e)
+            {
+                _logger.error("Could not close statement", e);
+            }
+            
+            try
+            {
+                if (oConnection != null)
+                    oConnection.close();
+            }
+            catch (Exception e)
+            {
+                _logger.error("Could not close connection", e);
+            }
+        }
+        return vAttributeNames;
+    }
+
+    /**
+     * Matches the supplied attribute name with the policy.
+     *
+     * Matches agains the configured attribute names or wildcard combination:
+     * <ul>
+     * <li>*</li>
+     * <li>*[matching chars]</li>
+     * <li>*[matching chars]*</li>
+     * <li>[matching chars]*</li> 
+     * <li>exact match</li>
+     * </ul>
+     * @param sName the name to match the policy
+     * @return TRUE if the attribute matches the policy
+     */
+    private boolean matches(String sName)
+    {
+        if (_vAttributeNames.contains(sName))
+            return true;
+            
+        for (String sReleaseName: _vAttributeNames)
+        {
+            int iWildcard = sReleaseName.indexOf("*");
+            if (iWildcard == 0)
+            {
+                String sEnd = sReleaseName.substring(1, sReleaseName.length());
+                if (sEnd.length() == 0)//support: *
+                    return true;
+                else if (sName.endsWith(sEnd))//support: *[name]
+                    return true;
+                else if (sEnd.endsWith("*"))
+                {
+                    String sIntermediate = sEnd.substring(0, sEnd.length() - 1);
+                    if (sName.contains(sIntermediate))//support: *[name]*
+                        return true;
+                }
+            }  
+            else if (iWildcard == sReleaseName.length()-1)
+            {
+                String sStart = sReleaseName.substring(0, iWildcard);
+                if (sName.startsWith(sStart))//support: [name]*
+                    return true;
+            }
+        }
+        return false;
+    }
+}
