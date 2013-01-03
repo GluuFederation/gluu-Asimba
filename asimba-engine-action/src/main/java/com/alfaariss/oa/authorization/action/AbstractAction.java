b9d853eec195b9a0d88e03cbff02ca33362b68f5
mdobrinic <mdobrinic@2aca1b3b-1dd8-4678-8513-65c153b8ef79>
2013-01-03 15:09:47 +0000

    add asimba-engine-action to baseline and distribution
    git-svn-id: svn://svn.code.sf.net/p/asimba/code/trunk@44 2aca1b3b-1dd8-4678-8513-65c153b8ef79

---
+++ asimba-engine-action/src/main/java/com/alfaariss/oa/authorization/action/AbstractAction.java
@@ -0,0 +1,142 @@
+/*
+ * Asimba - Serious Open Source SSO
+ * 
+ * Copyright (C) 2012 Asimba
+ * Copyright (C) 2007-2008 Alfa & Ariss B.V.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see www.gnu.org/licenses
+ * 
+ * Asimba - Serious Open Source SSO - More information on www.asimba.org
+ * 
+ */
+package com.alfaariss.oa.authorization.action;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Element;
+
+import com.alfaariss.oa.OAException;
+import com.alfaariss.oa.SystemErrors;
+import com.alfaariss.oa.api.authorization.IAuthorizationAction;
+import com.alfaariss.oa.api.configuration.IConfigurationManager;
+import com.alfaariss.oa.engine.core.attribute.AttributeException;
+
+/**
+ * Provides standard functionality used by all actions.
+ *
+ * This class is responsible for reading the mandatory parts of action configuration,
+ * such as the 'enabled' status and the action ID. 
+ *  
+ * @author JRE
+ * @author Alfa & Ariss
+ *
+ */
+public abstract class AbstractAction implements IAuthorizationAction
+{
+    /** The configuration manager */
+    protected IConfigurationManager _configManager = null;
+
+    private Log _logger;
+    private boolean _bIsEnabled;
+    private String _sActionID;
+    private String _sFriendlyName;
+    
+    /**
+     * Default constructor
+     */
+    public AbstractAction() 
+    {
+        _logger = LogFactory.getLog(AbstractAction.class); 
+        _bIsEnabled = false;
+    }
+
+    /**
+     * @see IAuthorizationAction#start(IConfigurationManager, org.w3c.dom.Element)
+     */
+    public void start(IConfigurationManager oConfigurationManager,
+        Element eConfig) throws OAException
+    {
+        if(eConfig == null)
+            throw new IllegalArgumentException(
+                "Supplied configuration section empty");
+        if (oConfigurationManager == null)
+            throw new IllegalArgumentException(
+                "Supplied configurationmanager empty");
+        
+        _configManager = oConfigurationManager;
+        
+        _bIsEnabled = true;
+        String sEnabled = _configManager.getParam(eConfig, "enabled");
+        if (sEnabled != null)
+        {
+            if (sEnabled.equalsIgnoreCase("FALSE"))
+                _bIsEnabled = false;
+            else if (sEnabled.equalsIgnoreCase("TRUE"))
+            {
+                _bIsEnabled = true;
+            }
+            else
+            {
+                _logger.error("Unknown value in 'enabled' action configuration item: " + sEnabled);
+                throw new AttributeException(SystemErrors.ERROR_CONFIG_READ);
+            }
+        }
+        
+        _sActionID = _configManager.getParam(eConfig, "id");
+        if (_sActionID == null || "".equals(_sActionID))
+        {
+            _logger.error("No id found for authorization action");
+            throw new AttributeException(SystemErrors.ERROR_CONFIG_READ);
+         }
+        
+        _sFriendlyName = _configManager.getParam(eConfig, "friendlyname");
+        if (_sFriendlyName == null)
+        {
+            _logger.error("No 'friendlyname' parameter found in action configuration");
+            throw new OAException(SystemErrors.ERROR_CONFIG_READ);
+        }
+    }
+
+    /**
+     * @see com.alfaariss.oa.api.authorization.IAuthorizationAction#stop()
+     */
+    public void stop()
+    {
+        _bIsEnabled = false;
+    }
+    
+    /**
+     * @see com.alfaariss.oa.api.authorization.IAuthorizationAction#getID()
+     */
+    public String getID()
+    {
+        return _sActionID;
+    }
+
+    /**
+     * @see com.alfaariss.oa.api.IManagebleItem#getFriendlyName()
+     */
+    public String getFriendlyName()
+    {
+        return _sFriendlyName;
+    }
+
+    /**
+     * @see com.alfaariss.oa.api.IManagebleItem#isEnabled()
+     */
+    public boolean isEnabled()
+    {
+        return _bIsEnabled;
+    }
+}
---
+++ asimba-engine-action/src/main/java/com/alfaariss/oa/authorization/action/ForceProfileAction.java
@@ -0,0 +1,129 @@
+/*
+ * Asimba - Serious Open Source SSO
+ * 
+ * Copyright (C) 2012 Asimba
+ * Copyright (C) 2007-2008 Alfa & Ariss B.V.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see www.gnu.org/licenses
+ * 
+ * Asimba - Serious Open Source SSO - More information on www.asimba.org
+ * 
+ */
+package com.alfaariss.oa.authorization.action;
+
+import java.util.List;
+import java.util.Vector;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Element;
+
+import com.alfaariss.oa.OAException;
+import com.alfaariss.oa.SystemErrors;
+import com.alfaariss.oa.UserEvent;
+import com.alfaariss.oa.api.authentication.IAuthenticationProfile;
+import com.alfaariss.oa.api.configuration.IConfigurationManager;
+import com.alfaariss.oa.api.session.ISession;
+import com.alfaariss.oa.engine.core.Engine;
+import com.alfaariss.oa.engine.core.authentication.AuthenticationProfile;
+import com.alfaariss.oa.engine.core.authentication.factory.IAuthenticationProfileFactory;
+
+/**
+ * Action that changes the user authentication profile to the configured profile.
+ *
+ * @author MHO
+ * @author JRE
+ * @author Alfa & Ariss
+ *
+ */
+public class ForceProfileAction extends AbstractAction
+{
+    private Log _logger;
+    private IAuthenticationProfileFactory _authenticationProfileFactory;
+    private String _sForceProfile;
+
+    /**
+     * Constructor
+     */
+    public ForceProfileAction()
+    {
+        _logger = LogFactory.getLog(ForceProfileAction.class); 
+        _sForceProfile = null;
+    }
+    
+    /**
+     * @see com.alfaariss.oa.api.authorization.IAuthorizationAction#perform(com.alfaariss.oa.api.session.ISession)
+     */
+    public UserEvent perform(ISession oSession) throws OAException
+    {
+        AuthenticationProfile authNProfile = _authenticationProfileFactory.getProfile(_sForceProfile);
+        if (authNProfile == null)
+        {
+            _logger.error("Unknown Authentication Profile configured: " + _sForceProfile);
+            throw new OAException(SystemErrors.ERROR_INTERNAL);
+        }
+        
+        if (!authNProfile.isEnabled())
+        {
+            _logger.error("Configured Authentication Profile is disabled: " + _sForceProfile);
+            throw new OAException(SystemErrors.ERROR_INTERNAL);
+        }
+        
+        List<IAuthenticationProfile> listProfiles = new Vector<IAuthenticationProfile>();
+        listProfiles.add(authNProfile);
+        
+        oSession.setAuthNProfiles(listProfiles);
+        return UserEvent.AUTHZ_METHOD_SUCCESSFUL;
+    }
+
+    /**
+     * Extra start method that is used to read the profile ID.
+     * 
+     * @see AbstractAction#start(IConfigurationManager, org.w3c.dom.Element)
+     */
+    public void start(IConfigurationManager oConfigurationManager,
+        Element eConfig) throws OAException
+    {
+        super.start(oConfigurationManager, eConfig);
+        
+        Element eProfile = _configManager.getSection(eConfig, "forcedprofile");
+        if (eProfile == null)
+        {
+            _logger.error("No 'forcedprofile' section found in action configuration");
+            throw new OAException(SystemErrors.ERROR_CONFIG_READ);
+        }
+        
+        _sForceProfile = _configManager.getParam(eProfile, "id");
+        if (_sForceProfile == null)
+        {
+            _logger.error("No config item 'profile' in Pre authR action configuration");
+            throw new OAException(SystemErrors.ERROR_CONFIG_READ);
+        }
+
+        _authenticationProfileFactory = Engine.getInstance().getAuthenticationProfileFactory();
+        
+        AuthenticationProfile authNProfile = _authenticationProfileFactory.getProfile(_sForceProfile);
+        if (authNProfile == null)
+        {
+            _logger.error("Unknown Authentication Profile configured: " + _sForceProfile);
+            throw new OAException(SystemErrors.ERROR_INIT);
+        }
+        
+        if (!authNProfile.isEnabled())
+        {
+            _logger.warn("Configured Authentication Profile is disabled: " + _sForceProfile);
+        }
+    }
+
+}
