7b32e400b5d0bf9610b5313e5209ed6823f578bb
Dmitry Ognyannikov <dmitry.ogn@gmail.com>
2015-10-08 16:23:10 -0300

    License change back.

--- asimba-utility/src/main/java/org/gluu/asimba/util/ldap/LdapConfigurationEntry.java
+++ asimba-utility/src/main/java/org/gluu/asimba/util/ldap/LdapConfigurationEntry.java
@@ -1,7 +1,25 @@
 /*
- * oxAsimba is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
- *
+ * Asimba Server
+ * 
  * Copyright (c) 2015, Gluu
+ * Copyright (C) 2013 Asimba
+ * Copyright (C) 2007-2008 Alfa & Ariss B.V.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see www.gnu.org/licenses
+ * 
+ * Asimba - Serious Open Source SSO - More information on www.asimba.org
+ * 
  */
 package org.gluu.asimba.util.ldap;
 
