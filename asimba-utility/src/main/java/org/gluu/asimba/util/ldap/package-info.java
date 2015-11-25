cc1aea4e0bc3e04faf78c16485cc0fbe913a9ab5
Dmitry Ognyannikov <dmitry.ogn@gmail.com>
2015-11-25 14:06:44 -0300

    Gluu source header changed

---
+++ asimba-utility/src/main/java/org/gluu/asimba/util/ldap/package-info.java
@@ -0,0 +1,28 @@
+/*
+ * Asimba Server
+ * 
+ * Copyright (c) 2015, Gluu
+ * Copyright (C) 2013 Asimba
+ * Copyright (C) 2007-2008 Alfa & Ariss B.V.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see www.gnu.org/licenses
+ * 
+ * gluu-Asimba - Serious Open Source SSO - More information on www.gluu.org
+ * 
+ */
+
+/**
+ * Support LDAP as configuration source. 
+ */
+package org.gluu.asimba.util.ldap;
